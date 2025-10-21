285a72df3406e057d58c0ba3243cfd8a8552d370
oorongtee <ray841206@gmail.com>
2025-10-21 17:54:22 +0800

    deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", './src/expo.html'],
+  content: ["./src/**/*.{html,js}", './src/index.html'],
   theme: {
     extend: {},
   },
